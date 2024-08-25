e8a3a158a9e56171b39e82f5fd94a984cb085a7e
MHKAPR <119296844+MhkaPR@users.noreply.github.com>
2024-08-25 19:13:27 +0330

    hasConflict and hasConflictLabTime wrote

--- src/main/java/ir/mhkapr/myscheduleapp/services/ScheduleService.java
+++ src/main/java/ir/mhkapr/myscheduleapp/services/ScheduleService.java
@@ -29,6 +29,94 @@ public class ScheduleService {
 
     }
 
+    private Boolean doLessonsOverlap(Lesson lesson1, Lesson lesson2) {
+
+        //first by first
+        if (lesson1.getDay1().equals(lesson2.getDay1())) {
+            if (lesson1.getHour1().equals(lesson2.getHour1())) {
+                if (lesson1.getEachOtherWeek1() == 1 && lesson2.getEachOtherWeek1() == 1) {
+                    return true;
+                } else if (lesson1.getEachOtherWeek1() == 2 && lesson2.getEachOtherWeek1() == 2) {
+                    return true;
+                }
+            }
+        }
+        //secend by first
+        if (lesson1.getDay2().equals(lesson2.getDay1())) {
+            if (lesson1.getHour2().equals(lesson2.getHour1())) {
+                if (lesson1.getEachOtherWeek1() == 1 && lesson2.getEachOtherWeek1() == 1) {
+                    return true;
+                } else if (lesson1.getEachOtherWeek1() == 2 && lesson2.getEachOtherWeek1() == 2) {
+                    return true;
+                }
+            }
+        }
+        //first by secend
+        if (lesson1.getDay1().equals(lesson2.getDay2())) {
+            if (lesson1.getHour1().equals(lesson2.getHour2())) {
+                if (lesson1.getEachOtherWeek1() == 1 && lesson2.getEachOtherWeek1() == 1) {
+                    return true;
+                } else if (lesson1.getEachOtherWeek1() == 2 && lesson2.getEachOtherWeek1() == 2) {
+                    return true;
+                }
+            }
+        }
+        //secend by secend
+        if (lesson1.getDay2().equals(lesson2.getDay2())) {
+            if (lesson1.getHour2().equals(lesson2.getHour2())) {
+                if (lesson1.getEachOtherWeek1() == 1 && lesson2.getEachOtherWeek1() == 1) {
+                    return true;
+                } else if (lesson1.getEachOtherWeek1() == 2 && lesson2.getEachOtherWeek1() == 2) {
+                    return true;
+                }
+            }
+        }
+        return hasConflictLabTime(lesson1, lesson2);
+    }
+
+    private Boolean hasConflictLabTime(Lesson lesson1, Lesson lesson2) {
+        if (!lesson1.getHasLab() && !lesson2.getHasLab()) return false;
+        else {
+            //first lab by first
+            if (lesson1.getHasLab()) {
+                if (lesson1.getLabDay().equals(lesson2.getDay1())) {
+                    if (lesson1.getLabHour().equals(lesson2.getHour1())) {
+                        return true;
+                    }
+                }
+                //first lab by secend
+                if (lesson1.getLabDay().equals(lesson2.getDay2())) {
+                    if (lesson1.getLabHour().equals(lesson2.getHour2())) {
+                        return true;
+                    }
+                }
+            }
+
+            if (lesson2.getHasLab()) {
+                //first by secend lab
+                if (lesson1.getDay1().equals(lesson2.getLabDay())) {
+                    if (lesson1.getHour1().equals(lesson2.getLabHour())) {
+                        return true;
+                    }
+                }
+                //secend by secend lab
+                if (lesson1.getDay2().equals(lesson2.getLabDay())) {
+                    if (lesson1.getHour2().equals(lesson2.getLabHour())) {
+                        return true;
+                    }
+                }
+            }
+
+            if (lesson1.getHasLab() && lesson2.getHasLab()) {
+                if (lesson1.getLabDay().equals(lesson2.getLabDay())) {
+                    if (lesson1.getLabHour().equals(lesson2.getLabHour())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
     private Boolean hasManyReligiousLessons(List<Lesson> lessonList) {
         return lessonList.stream().filter(Lesson::getIsReligious).count() > 1;
     }
