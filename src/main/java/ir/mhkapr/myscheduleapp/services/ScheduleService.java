f3ea5307106bcaa542ab2ab42a2386bd85f8972d
MHKAPR <119296844+MhkaPR@users.noreply.github.com>
2024-08-25 15:02:17 +0330

    method of powerSet completed

--- src/main/java/ir/mhkapr/myscheduleapp/services/ScheduleService.java
+++ src/main/java/ir/mhkapr/myscheduleapp/services/ScheduleService.java
@@ -1,9 +1,19 @@
 package ir.mhkapr.myscheduleapp.services;
 
+import ir.mhkapr.myscheduleapp.objects.Lesson;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
+    private List<List<Lesson>> powerSet(List<Lesson> lessonList, Integer min, Integer max) {
+        SetService setService = new SetService(lessonList);
+        List<List<Integer>> ListOfIndexPlans = setService.calculateByRange(min, max);
+        return ListOfIndexPlans.stream()
+                .map(indexList -> indexList.stream().map(lessonList::get).toList()).toList();
+    }
+
 }
