c72003fadc87b850445809f04aca1f7b02efe260
MHKAPR <119296844+mhkapr@users.noreply.github.com>
2024-08-25 14:44:18 +0330

    feature of set service completed

---
+++ src/main/java/ir/mhkapr/myscheduleapp/services/ScheduleService.java
@@ -0,0 +1,9 @@
+package ir.mhkapr.myscheduleapp.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+}
